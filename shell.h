faae7894e6c46a56d0359ad8d8fb459db09ccb38
MsOwuor <immaculateadhiambo79@gmail.com>
2023-08-15 04:11:47 -0700

    update

---
+++ shell.h
@@ -0,0 +1,34 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <wait.h>
+#include <fcntl.h>
+#include <dirent.h>
+#include <signal.h>
+
+/**
+ * struct list - singly linked list
+ * @ptr: a malloced address
+ * @next: points to the next node
+ * Description: singly linked list node structure
+ *
+ */
+
+typedef struct list
+{
+	void *ptr;
+	struct list *next;
+} list_t;
+
+char *_strcat(char *dest, char *src);
+int _strcmp(char *s1, char *s2);
+char *_strcpy(char *dest, char *src);
+
+#endif /* SHELL_H */
